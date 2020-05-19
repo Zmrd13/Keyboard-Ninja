37b5447d1079ab6e5e11b7c71d7489477bf8d7fa
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2020-05-20 05:04:13 +0700

    Compile gtest .cc files

--- tests/install.sh
+++ tests/install.sh
@@ -1,5 +1,5 @@
 echo "Compiling gtests ..."
-g++ -Wall -Werror -std=c++1z -I. -Lgtest/lib main.cpp -o Gtests.exe \
+g++ -Wall -Werror -std=c++1z -Igoogletest/include -Igoogletest -Lgoogletest/lib googletest/src/gtest-all.cc main.cpp -o Gtests.exe \
 	LevelFileReaderTest.cpp ../lib/LevelFileReader.cpp \
 	ArrProcTest.cpp \
 -lgtest -pthread
