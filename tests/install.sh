ef02327ace745f64b19b39ba6d86ede20b18df1d
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2020-05-20 05:11:54 +0700

    Update install.sh

--- tests/install.sh
+++ tests/install.sh
@@ -1,5 +1,6 @@
 echo "Compiling gtests ..."
-g++ -Wall -Werror -std=c++1z -Igoogletest/include -Igoogletest -Lgoogletest/lib googletest/src/gtest-all.cc main.cpp -o Gtests.exe \
+g++ -Wall -Werror -std=c++1z -Igoogletest/include -Igoogletest -Lgoogletest/lib -o Gtests.exe \
+	googletest/lib/libgtest.a \
+	googletest/src/gtest-all.cc main.cpp \
 	LevelFileReaderTest.cpp ../lib/LevelFileReader.cpp \
-	ArrProcTest.cpp \
--lgtest -pthread
+	ArrProcTest.cpp -pthread
