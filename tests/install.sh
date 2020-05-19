8bcb5fbce417366b7b57da07d9df114d111a4a5b
Egor <60730567+Zmrd13@users.noreply.github.com>
2020-05-20 03:12:43 +0700

    Update install.sh

--- tests/install.sh
+++ tests/install.sh
@@ -1,5 +1,5 @@
 echo "Compiling gtests ..."
-g++ -Wall -Werror -I. -Lgtest/lib main.cpp -o Gtests.exe \
+g++ -Wall -Werror -I. -std=c11 -Lgtest/lib main.cpp -o Gtests.exe \
 	LevelFileReaderTest.cpp ../lib/LevelFileReader.cpp \
 	ArrProcTest.cpp \
 -lgtest -pthread
