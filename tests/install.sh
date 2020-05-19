45e21d838601ff188633b4c5de19f8fbeb7c9749
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2020-05-20 04:44:14 +0700

    Change c++ standart to -std=c++1z

--- tests/install.sh
+++ tests/install.sh
@@ -1,5 +1,5 @@
 echo "Compiling gtests ..."
-g++ -Wall -Werror -std=c++17 -I. -Lgtest/lib main.cpp -o Gtests.exe \
+g++ -Wall -Werror -std=c++1z -I. -Lgtest/lib main.cpp -o Gtests.exe \
 	LevelFileReaderTest.cpp ../lib/LevelFileReader.cpp \
 	ArrProcTest.cpp \
 -lgtest -pthread
